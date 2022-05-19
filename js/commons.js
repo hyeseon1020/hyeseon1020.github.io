19225adf0df046b205faefccc17e8b06a78a457f
hyeseon1020 <gptjs6549@gmail.com>
2022-05-19 15:38:18 +0900

    menu click

--- js/commons.js
+++ js/commons.js
@@ -1,15 +1,33 @@
 $(function(){
+    //select
+    let label = $('label'),
+        select = $('select');
+    label.click(function(){
+        $(this).toggleClass('rotate');
+    });
+    // menu
     let menuBtn = $('header div .j-betweent a:nth-of-type(1)'),
         menuWrap = $('#menu_wrapper'),
         menu = $('#menu'),
-        close = menu.find('li:last-child');
-menuBtn.click(function(e){
-    e.preventDefault();
-    menuWrap.addClass('visible');
-});
-close.click((e)=>{
-    e.preventDefault();
-    menuWrap.removeClass('visible');
-});
+        menuItem = menu.find('li');
+
+    menuBtn.click((e)=>{
+        e.preventDefault();
+        menuWrap.addClass('visible');
+    });
+    menu.find('.close').click((e)=>{
+        e.preventDefault();
+        menuWrap.removeClass('visible');
+    });
+
+    menuItem.click(function(e){
+        e.preventDefault();
+        let targetId = $(this).find('a').attr('href');
+        $(targetId).addClass('visible');
+        $(targetId).find('.close').click((e)=>{
+            e.preventDefault();
+            $(targetId).removeClass('visible');
+        });
+    });
 
 });//script
