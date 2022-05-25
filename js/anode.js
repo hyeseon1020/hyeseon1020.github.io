becab932b87858176058dfc08479d3de41597352
hyeseon1020 <gptjs6549@gmail.com>
2022-05-25 22:49:45 +0900

    모달 js

--- js/anode.js
+++ js/anode.js
@@ -1,4 +1,5 @@
 $(function(){
+    // error 
     $("#queryForm").validate({
         rules: {
             email: {
@@ -15,6 +16,40 @@ $(function(){
         errorClass:'bad-input',
         errorElement: "span"
     });
+    // close
+    let nav = $('.query-nav a'),
+        query = $('#query'),
+        x = query.find('i');
+        
+    nav.click((e)=>{
+        e.preventDefault();
+        query.removeClass('hidden');
+        $('body').addClass('flow');
+    });
+    x.click(()=>{
+        query.addClass('hidden');
+        $('body').removeClass('flow');
+    });
+    // radio
+    let cbinput =  $('.check-box input');
 
-
+    cbinput.click(function(){
+        cbinput.siblings('label').removeClass('check');
+        $(this).siblings('label').addClass('check');
+    });
+    // select
+    let selec = $('.select'),
+        btn = selec.find('.label'),
+        rotate = selec.find('i'),
+        option = selec.find('.option');
+    console.log(rotate);
+    btn.click(()=>{
+        selec.toggleClass('rotate');
+    });
+    option.click(function(){
+        let otn = $(this).html();
+        btn.html(otn);
+        selec.find('span').toggleClass('rotate');
+        selec.toggleClass('rotate');
+    });
 });
