d0cf88809f87fcd21c65928efb947f80377d9575
hyeseon1020 <gptjs6549@gmail.com>
2022-05-27 23:53:45 +0900

    loop 추가  + 예전 js 지움

--- js/main.js
+++ js/main.js
@@ -1,54 +1,17 @@
 $(document).ready(function(){
-    let section = $('.scroll-wrapper section');
+    let section = $('#fullpage section');
     let more = section.find('span');
     let pager = $('.pager a');
 
-    section.eq(0).find('.container').addClass('active');
-    pager.click(function(e){
-        e.preventDefault();
-        let targetId = $(this).attr('href');
-        let targetSCT = $(targetId).offset().top - 190;
-        $('html,body').animate({scrollTop:targetSCT}, 400);
-        let idx = $(this).index();
-        section.siblings().find('.container').removeClass('active');
-        section.eq(idx).find('.container').addClass('active');
+    $('#fullpage').fullpage({
+        //options here
+        autoScrolling:true,
+        scrollHorizontally: true,
+        navigation: true,
+        loopBottom:true,
+        loopTop:true
     });
     
-    $(document).ready(function(){
-        let section = $('#fullpage section');
-        let more = section.find('span');
-    
-        $('#fullpage').fullpage({
-            //options here
-            autoScrolling:true,
-            scrollHorizontally: true,
-            navigation: true,
-        });
-    
-        // more
-        more.find('a').click(function(e){
-            e.preventDefault();
-            let targetId = $(this).attr('href');
-            $(targetId).addClass('visible');
-            $(targetId).find('.close').click((e)=>{
-                e.preventDefault();
-                $(targetId).removeClass('visible');
-            });
-        });
-    });//script
-    $(window).scroll(function(){
-        let sct = $(this).scrollTop();
-        let sections = $('.scroll');
-        sections.each(function(idx){
-            if(sct >= $(this).offset().top - 200){
-                pager.removeClass('on');
-                pager.eq(idx).addClass('on');
-                section.siblings().find('.container').removeClass('active');
-                section.eq(idx).find('.container').addClass('active');
-            }
-          });
-    });
-
     more.find('a').click(function(e){
         e.preventDefault();
         let targetId = $(this).attr('href');
