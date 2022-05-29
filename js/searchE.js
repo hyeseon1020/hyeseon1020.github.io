c3a7af126b2cc3d78d65412e67723e9eaf609528
hyeseon1020 <gptjs6549@gmail.com>
2022-05-29 23:33:00 +0900

    검색결과 영어 페이지

---
+++ js/searchE.js
@@ -0,0 +1,37 @@
+$(function(){
+    //search
+    let searchBtn = $('header .search-btn'),
+        search = $('header .search');
+    searchBtn.click((e)=>{
+        e.preventDefault();
+        if(search.hasClass('hidden')){
+            $('body').css({overflow:'auto'});
+        }else{
+            $('body').css({overflow:'hidden'});
+        }
+    });
+
+    let tabMenu = $('.tab-menu li'),
+        tabResult = $('.tab-result > div'),
+        result = $('.result-leng');
+
+    tabResult.eq(3).show();
+    let resultLeng = tabResult.eq(3).find('.content').length;
+    result.find('b').html(`'${resultLeng}'`);
+
+    tabMenu.click(function(e){
+        e.preventDefault();
+        tabMenu.find('a').removeClass('target');
+        $(this).find('a').addClass('target');
+        let targetId = $(this).find('a').attr('href');
+        let resultLeng = $(targetId).find('.content').length;
+        console.log(resultLeng);
+        result.find('b').html(`'${resultLeng}'`);
+        activateTab($(this).index());
+    });
+    function activateTab(idx){
+        tabResult.hide();
+        tabResult.eq(idx).show();
+    }
+    
+}); //ready
