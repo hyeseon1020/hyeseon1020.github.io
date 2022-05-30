a3fab60b41942365d3d1b366bddeac2d4ff5f535
hyeseon1020 <gptjs6549@gmail.com>
2022-05-30 17:20:58 +0900

    form

--- js/anode.js
+++ js/anode.js
@@ -23,12 +23,18 @@ $(function(){
             email: {
                 required: true,
                 email: true
+            },
+            type:{
+                required: true,
             }
         },
         messages:{
             email: {
                 required: '입력하신 메일로 답변이 발송되오니 메일 주소를 정확하게 기재해주세요.',
                 email: '이메일 형식에 맞춰 입력하세요 ex) abc@gmail.com'
+            },
+            type:{
+                required: '문의유형을 선택해주세요. ',
             }
         },
         errorClass:'bad-input',
