ad6a5db7e4eb45a546af2651bee2e2d8d3c59301
hyeseon1020 <gptjs6549@gmail.com>
2022-06-06 20:38:51 +0900

    웹표준 수정

--- js/search.js
+++ js/search.js
@@ -23,13 +23,49 @@ $(function(){
         $(this).find('a').addClass('target');
         let targetId = $(this).find('a').attr('href');
         let resultLeng = $(targetId).find('.content').length;
-        console.log(resultLeng);
         result.find('b').html(`'${resultLeng}'`);
+        console.log(resultLeng);
         activateTab($(this).index());
     });
+
     function activateTab(idx){
         tabResult.hide();
         tabResult.eq(idx).show();
     }
-    
+
+    //pagination a
+    let splitPage = 8;
+    let content = $('#a .content');
+    let contentLeng = content.length;
+    let pageCount = Math.ceil(contentLeng/splitPage);
+    let numbers = $('#a .numbers');
+    console.log(numbers);
+
+    for(let i = 1; i<=pageCount; i++){
+        numbers.append(`<li><a href="">${i}</a></li>`);
+    }
+
+    numbers.find('li:first-child a').addClass('active');
+
+    function dispalyContents(idx){
+        let start = idx*splitPage;
+        let end = start + splitPage;
+
+        content.hide();
+        content.slice(start,end).show();
+    }
+    dispalyContents(0);
+
+    let numbersLi = $('.numbers li');
+    numbersLi.click(function(e){
+        e.preventDefault();
+        numbers.find('a').removeClass('active');
+        $(this).find('a').addClass('active');
+        
+        
+        let targetIndex = $(this).index();
+        console.log(targetIndex);
+        dispalyContents(targetIndex);
+    });
+
 }); //ready
